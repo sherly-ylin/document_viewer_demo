6502f05eee32c50681ec1db7331078d16feca6cc
Yixuan Sherly Lin <linyixuansherly@gmail.com>
2025-06-20 09:32:49 -0400

    Create project

---
+++ document_viewer_demo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace document_viewer_demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
